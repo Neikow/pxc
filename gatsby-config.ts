39ccdd4bbde941e34ca7fe66a8ff8d8c626ab4c0
Vitaly <50383391+Neikow@users.noreply.github.com>
2023-01-05 09:36:38 +0100

    added path prefix for github pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `pxc`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: `/pxc`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
